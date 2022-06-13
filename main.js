097d75d2c98a2fcde1e54c95a1852822ae3d3c04
kochnovdenis <56402259+kochnovdenis@users.noreply.github.com>
2022-06-13 15:09:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
   );
   
   initRussianpostOps(total);
+  initRussianpostCourier(total);
 
   $('h3.cli-cart-total').on('DOMSubtreeModified', function () {
     if (!$(this).text()) {
@@ -20,6 +21,7 @@ $(document).ready(function() {
     total = parseInt($(this).text().replace(/[^\d.]/g, ''), 10) * 100;
 
     initRussianpostOps(total);
+    initRussianpostCourier(total);
   });
 });
 
@@ -35,6 +37,20 @@ function initRussianpostOps(total) {
     };
 
   ecomStartWidget(params);
+}
+
+function initRussianpostCourier(total) {
+  $('[data-id="8b8fc310-e9c0-11ec-ba06-654ee29d1609"]').val('');
+  $('#ecom-widget-courier').html('');
+  
+  var params = {
+      id: 28356,
+      callbackFunction: fillAddress,
+      containerId: 'ecom-widget-courier',
+      sumoc: total
+    };
+
+  courierStartWidget(params);
 }
 
 function fillAddress(data) {
