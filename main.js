761fe4dcb0974ebdb86719e803e0047e96e7e809
kochnovdenis <56402259+kochnovdenis@users.noreply.github.com>
2022-06-16 22:01:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ function initRussianpostCourier(total) {
 
 function fillAddress(data) {
   console.info(data);
+  console.info('test');
   
   var address = [];
   for (prop of ['regionTo', 'areaTo', 'cityTo', 'addressTo', 'indexTo']) {
